9f1bdf3867f9604519ddd6ede05bddcf2f9e702c
Vika <zanuda999@bitbucket.org>
2014-04-24 01:22:45 +0300

    change to using arrows icon from new bootstrap

--- datetimewidget/static/js/bootstrap-datetimepicker.js
+++ datetimewidget/static/js/bootstrap-datetimepicker.js
@@ -154,7 +154,7 @@
 		if (this.isRTL){
 			this.picker.addClass('datetimepicker-rtl');
 			this.picker.find('.prev i, .next i')
-						.toggleClass('icon-arrow-left icon-arrow-right');
+						.toggleClass('glyphicon glyphicon-arrow-left glyphicon glyphicon-arrow-right');
 		}
 		$(document).on('mousedown', function (e) {
 			// Clicked outside the datetimepicker, hide it
@@ -1522,9 +1522,9 @@
 		},
 		headTemplate: '<thead>'+
 							'<tr>'+
-								'<th class="prev"><i class="icon-arrow-left"/></th>'+
+								'<th class="prev"><i class="glyphicon glyphicon-arrow-left"/></th>'+
 								'<th colspan="5" class="switch"></th>'+
-								'<th class="next"><i class="icon-arrow-right"/></th>'+
+								'<th class="next"><i class="glyphicon glyphicon-arrow-right"/></th>'+
 							'</tr>'+
 						'</thead>',
 		contTemplate: '<tbody><tr><td colspan="7"></td></tr></tbody>',
